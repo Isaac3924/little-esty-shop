dff1ba4d4a2b21781b70f030f3e8ef6ee8a5392b
Isaac Alter <i_alter@yahoo.com>
2023-02-28 14:48:54 -0700

    Adds feat: a lot of logic for the User Story 30 model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Merchant, type: :model do
   let!(:this_gai_ovah_hea) { Customer.create!(first_name: "Dis", last_name: "Gai") }
   let!(:nkelthuraksyyll) { Customer.create!(first_name: "N'kelthuraksyyll", last_name: "The unboud, Lord of ten Thousand chains unBroken, Vanquisher of KMart") }
   let!(:phil) { Customer.create!(first_name: "Phil", last_name: "Phil") } 
+  let!(:ahl) { Customer.create!(first_name: "Bai", last_name: "Ahl") } 
 
   let!(:invoice1) { Invoice.create!(customer_id: this_gai_ovah_hea.id, created_at: Date.yesterday) } 
   let!(:invoice2) { Invoice.create!(customer_id: this_gai_ovah_hea.id,  created_at: Date.tomorrow) } 
@@ -19,10 +20,27 @@ RSpec.describe Merchant, type: :model do
   let!(:invoice_vinyl) {Invoice.create!(customer_id: nkelthuraksyyll.id, status: "completed") }
   let!(:invoice_lunchbox) {Invoice.create!(customer_id: phil.id, status: "cancelled") }
   let!(:invoice_macguffin_muffins) {Invoice.create!(customer_id: phil.id, status: "completed") }
+  let!(:invoice_lipstick) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_ring) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_stuff) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_pizza) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_pizza_fort) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_fuzzy_pizza) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_cream) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_ice) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_schnitzel) {Invoice.create!(customer_id: ahl.id, status: "completed") }
+  let!(:invoice_taco_pizza) {Invoice.create!(customer_id: ahl.id, status: "completed") }
 
   let!(:sam) { Merchant.create!(name: "Sam's Sports", status: 0) }
   let!(:bob) { Merchant.create!(name: "Bob's Beauties") }
   let!(:rob) { Merchant.create!(name: "Rob's Rarities") } 
+  let!(:things) { Merchant.create!(name: "Things 'n Stuff", status: 0) }
+  let!(:fatty) { Merchant.create!(name: "Fatty Fartbear's Pizza Town") } 
+  let!(:franklin) { Merchant.create!(name: "Franklin Facebear's Pizza Fortress") }
+  let!(:fuzzy) { Merchant.create!(name: "Fuzzy Fred's Pizorium Emporium", status: 0) }
+  let!(:butt) { Merchant.create!(name: "Butt Johnson's Tuna-Flavored Ice Cream Showdown") } 
+  let!(:eenus) { Merchant.create!(name: "Eenus & Benus' House of Weenuhs") }
+  let!(:combo) { Merchant.create!(name: "The Combination Pizza Hut and Taco Bell", status: 0) }
   
   let!(:football) { sam.items.create!(name: "Football", description: "This a football", unit_price: 3000, status: 0) }
   let!(:baseball) { sam.items.create!(name: "Baseball", description: "This a baseball", unit_price: 2500, status: 0) }
@@ -32,6 +50,16 @@ RSpec.describe Merchant, type: :model do
   let!(:vinyl) { sam.items.create!(name: "Unknown Vinyl", description: "A vinyl. Who knows what's on it?", unit_price: 999999) }
   let!(:lunchbox) { sam.items.create!(name: "Lunch Box", description: "Molded sandvich included", unit_price: 5693) }
   let!(:macguffin_muffins) { sam.items.create!(name: "The Macguffin Muffins", description: "https://youtu.be/di7DI6Q7JXA?t=39", unit_price: 99999999) }
+  let!(:lipstick) { bob.items.create!(name: "Lipstick", description: "It's red flavoured", unit_price: 4500, status: 0) }
+  let!(:ring) { rob.items.create!(name: "Emerald inset Ring", description: "Insert into small child's mouth", unit_price: 54705, status: 0) }
+  let!(:stuff) { things.items.create!(name: "Stuff", description: "Sometimes confused with things", unit_price: 400, status: 0) }
+  let!(:pizza) { fatty.items.create!(name: "Pizza", description: "Yum. If you dare.", unit_price: 6999, status: 0) }
+  let!(:pizza_fort) { franklin.items.create!(name: "Pizza Fort", description: "The foundations are made of cardboard, blood, and mozzarella", unit_price: 7650, status: 0) }
+  let!(:fuzzy_pizza) { fuzzy.items.create!(name: "Fuzzy Pizza", description: "It was clearly living.", unit_price: 3975, status: 0) }
+  let!(:cream) { butt.items.create!(name: "Cream Tuna", description: "It isn't ice cream, nor will it be. Yet we must face hubris directly.", unit_price: 30, status: 0) }
+  let!(:ice) { butt.items.create!(name: "Ice", description: "Add to cream.", unit_price: 40, status: 0) }
+  let!(:schnitzel) { eenus.items.create!(name: "Schnitzel", description: "Drei Bier sind auch ein Schnitzel", unit_price: 8960, status: 0) }
+  let!(:taco_pizza) { combo.items.create!(name: "Taco Pizza", description: "Pizza Taco", unit_price: 19999, status: 0) }
 
   let!(:transaction_football1) { invoice1.transactions.create!(result: 1) }
   let!(:transaction_baseball1) { invoice1.transactions.create!(result: 0) }
@@ -42,6 +70,16 @@ RSpec.describe Merchant, type: :model do
   let!(:transaction_vinyl1) { invoice_vinyl.transactions.create!(result: 0) }
   let!(:transaction_lunchbox1) { invoice_lunchbox.transactions.create!(result: 1) }
   let!(:transaction_macguffin_muffins1) { invoice_macguffin_muffins.transactions.create!(result: 0) }
+  let!(:transaction_lipstick1) { invoice_lipstick.transactions.create!(result: 0) }
+  let!(:transaction_ring1) { invoice_ring.transactions.create!(result: 0) }
+  let!(:transaction_stuff1) { invoice_stuff.transactions.create!(result: 0) }
+  let!(:transaction_pizza1) { invoice_pizza.transactions.create!(result: 0) }
+  let!(:transaction_pizza_fort1) { invoice_pizza_fort.transactions.create!(result: 0) }
+  let!(:transaction_fuzzy_pizza1) { invoice_fuzzy_pizza.transactions.create!(result: 0) }
+  let!(:transaction_cream1) { invoice_cream.transactions.create!(result: 0) }
+  let!(:transaction_ice1) { invoice_ice.transactions.create!(result: 0) }
+  let!(:transaction_schnitzel1) { invoice_schnitzel.transactions.create!(result: 0) }
+  let!(:transaction_taco_pizza1) { invoice_taco_pizza.transactions.create!(result: 0) }
 
   before (:each) do
     @football_inv = InvoiceItem.create!(invoice_id: invoice1.id, item_id: football.id, quantity: 1, unit_price: 3000, status: 0)   
@@ -53,6 +91,16 @@ RSpec.describe Merchant, type: :model do
     @vinyl_inv = InvoiceItem.create!(invoice: invoice_vinyl, item: vinyl, quantity: 3, unit_price: 9999999, status: 2)                          
     @lunchbox_inv = InvoiceItem.create!(invoice: invoice_lunchbox, item: lunchbox, quantity: 500, unit_price: 66666, status: 1)                    
     @macguffin_muffins_inv = InvoiceItem.create!(invoice: invoice_macguffin_muffins, item: macguffin_muffins, quantity: 7, unit_price: 99999, status: 0)    
+    @lipstick_inv = InvoiceItem.create!(invoice: invoice_lipstick, item: lipstick, status: 0) 
+    @ring_inv = InvoiceItem.create!(invoice: invoice_ring, item: ring, status: 0) 
+    @stuff_inv = InvoiceItem.create!(invoice: invoice_stuff, item: stuff, status: 0) 
+    @pizza_inv = InvoiceItem.create!(invoice: invoice_pizza, item: pizza, status: 0) 
+    @pizza_fort_inv = InvoiceItem.create!(invoice: invoice_pizza_fort, item: pizza_fort, status: 0) 
+    @fuzzy_pizza_inv = InvoiceItem.create!(invoice: invoice_fuzzy_pizza, item: fuzzy_pizza, status: 0) 
+    @cream_inv = InvoiceItem.create!(invoice: invoice_cream, item: cream, status: 0) 
+    @ice_inv = InvoiceItem.create!(invoice: invoice_ice, item: ice, status: 0) 
+    @schnitzel_inv = InvoiceItem.create!(invoice: invoice_schnitzel, item: schnitzel, status: 0) 
+    @taco_pizza_inv = InvoiceItem.create!(invoice: invoice_taco_pizza, item: taco_pizza, status: 0) 
   end
 
   describe 'instance methods' do
@@ -85,11 +133,17 @@ RSpec.describe Merchant, type: :model do
 
   describe '.scope' do 
     it '.disabled_merchants' do 
-      expect(Merchant.disabled_merchants).to eq([bob, rob])
+      expect(Merchant.disabled_merchants).to eq([bob, rob, fatty, franklin, butt, eenus])
     end
 
     it '.enabled_merchants' do 
-      expect(Merchant.enabled_merchants).to eq([sam])
+      expect(Merchant.enabled_merchants).to eq([sam, things, fuzzy, combo])
+    end
+  end
+
+  describe 'class methods' do
+    it '#top_5_merchants' do
+      expect(Merchant.top_5_merchants).to eq([sam])
     end
   end
 end
