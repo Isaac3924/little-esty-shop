0365f04adf5a97dd584b6347fc6a192192bde759
Bobby Luly <bobbyluly@gmail.com>
2023-02-28 19:20:35 -0700

    Feat- add @top_customers to index action in MerchDashboard controller, add favorite customers section to dashboard/index view

--- app/controllers/merchants/dashboard_controller.rb
+++ app/controllers/merchants/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Merchants::DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @ready_merchant_items = @merchant.items_not_yet_shipped
-    
+    @top_customers = @merchant.merchant_top_5_customers
   end
 end
