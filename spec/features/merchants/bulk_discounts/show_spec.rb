dc0ff8308d8b61b8bde796cc00930a44b7d4af78
Isaac Alter <i_alter@yahoo.com>
2023-03-04 21:15:14 -0700

    Adds feat: All tests needed for User Story 5

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe BulkDiscount, type: :feature do
     let!(:bulk_discount15) { ralph.bulk_discounts.create!(percentage_discount: 3, quantity_threshold: 17) }
 
     before (:each) do 
-      visit "/merchants/#{bob.id}/bulk_discounts/#{bulk_discount20.id}"
+      visit merchant_bulk_discount_path(bob, bulk_discount20)
     end
 
     describe 'As a merchant' do 
@@ -25,6 +25,38 @@ RSpec.describe BulkDiscount, type: :feature do
 
           expect(page).to_not have_content('Percentage Discount: 3%')
           expect(page).to_not have_content('Quantity Threshold: 17')
+        end
+
+        it "I see a link to edit the bulk discount" do
+          expect(page).to have_link('Edit')
+        end
+
+        it "When I click the Edit link, I am taken to a new page with a form to edit the bulk_discount and that the curren attributes are pre-populated in the form" do
+          click_link 'Edit'
+
+          expect(current_path).to eq(edit_merchant_bulk_discount_path(bob, bulk_discount20))
+
+          within ("section#edit_bulk_discount") do
+            expect(page).to have_field("Percentage Discount", with: '10')
+            expect(page).to have_field("Quantity Threshold", with: '20')
+          end
+        end
+
+        it "When I change any/all information and click submit, I am taken back to the show page and see the attributes have been updated" do
+          expect(page).to have_content('Percentage Discount: 10%')
+          expect(page).to have_content('Quantity Threshold: 20')
+
+          click_link 'Edit'
+
+          within ("section#edit_bulk_discount") do
+            fill_in 'Percentage Discount', with: 99
+            fill_in 'Quantity Threshold', with: 3
+            click_button 'Submit'
+          end
+
+          expect(current_path).to eq(merchant_bulk_discount_path(bob, bulk_discount20))
+          expect(page).to have_content('Percentage Discount: 99%')
+          expect(page).to have_content('Quantity Threshold: 3')
         end
       end
     end
