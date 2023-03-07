93c173f9a73fc7c8f1c110a36913f366d52e51fb
Isaac Alter <i_alter@yahoo.com>
2023-03-07 15:53:37 -0700

    Adds refactor: commented out old method that was detemining bulk_discount. Left in to show progression.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,16 +1,19 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_many :bulk_discounts, through: :item
   
   enum status: ["pending", "packaged", "shipped"]
 
-  def discount_check
-    item.merchant.bulk_discounts
-                 .where("quantity_threshold <= ?", quantity)
-                 .select('percentage_discount')
-                 .order('percentage_discount DESC')
-                 .limit(1)
-                 .select("bulk_discounts.*")
-                 .take  
-  end
+  #Commenting this out as it is old unneeded code, but leaving it in as reference to show what my thought process was originally.
+  #You can see the further logic in the invoice_item_spec file, those tests will also be commented out.
+  # def discount_check
+  #   item.merchant.bulk_discounts
+  #                .where("quantity_threshold <= ?", quantity)
+  #                .select('percentage_discount')
+  #                .order('percentage_discount DESC')
+  #                .limit(1)
+  #                .select("bulk_discounts.*")
+  #                .take  
+  # end
 end
