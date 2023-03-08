59d9d6ca76eda227dd92d038dc9d33b0394c6d30
Isaac Alter <i_alter@yahoo.com>
2023-03-08 05:13:38 -0700

    Adds refactor: made more scope methods within invoice_item model to be utilized within the invoice model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,6 +7,10 @@ class InvoiceItem < ApplicationRecord
 
   scope :revenue, -> { sum("unit_price * quantity") }
 
+  scope :check_quantity, -> { joins(:bulk_discounts).where('bulk_discounts.quantity_threshold <= invoice_items.quantity') }
+
+  scope :calculate_discounted_amount, -> { select("invoice_items.*, MAX((invoice_items.unit_price*invoice_items.quantity) * (bulk_discounts.percentage_discount/100.0)) AS discounted_amount") }
+  
   #Commenting this out as it is old unneeded code, but leaving it in as reference to show what my thought process was originally.
   #You can see the further logic in the invoice_item_spec file, those tests will also be commented out.
   # def discount_check
